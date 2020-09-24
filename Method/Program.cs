5e49f2c17c29646b53de147a9e67e49047c7459d
Alexandra Givins <agivins@usf.edu>
2020-09-24 14:03:42 -0700

    Name

---
+++ Method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PC
+{
+    class Program
+    {
+        static void UserName()
+        {
+            Console.WriteLine("Please enter your name..");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name..");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "  " + " Go Bus!");
+            {
+                Console.WriteLine(" The Program enter your name.");
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
+
+
+
