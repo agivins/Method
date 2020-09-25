5cc964e2e5aaa007a3fd4255343df47014169a40
Alexandra Givins <agivins@usf.edu>
2020-09-24 20:46:05 -0700

    Method_3A

--- Method/Program.cs
+++ Method/Program.cs
@@ -1,33 +1,72 @@
-﻿/*ISM 4300 Console Program- Submitted by Alexandra Givins
- Comments: This code demonstrates the use of Method statements after getting input from users. 
-*/
-using System;
+﻿using System;
 
-namespace PC
+
+
+namespace Method
+
 {
+
     class Program
+
     {
-        static void MyName()
+
+        static void User()
         {
+            // Ask the user for input
+
             Console.WriteLine("Please enter your name..");
+        
         }
-        static void Main(string[] args)
+        private static void Main(string[] args)
+
         {
-            //the value of user input 
+
+
+
+        /*
+
+        Use the cath block to validate user input.
+
+        */
+
+        try
+
+        {
+
+            // This variable will gather the user input
+
+            string input = Console.ReadLine();
+
+            
+
+           
             Console.WriteLine("Please enter your name..");
             string name = Console.ReadLine();
-            //Here is the for the use input 
-            Console.WriteLine("Hello" + name + "-" + "Go Bus!");
-            {
-                Console.WriteLine(" The Program enter your name.");
-                // Pause the program and wait for the user to press a key to end the program
-                Console.WriteLine("Press any key to exit the program...");
-                Console.ReadKey(true);
-            }
-        }
-    }
-}
 
+            Console.WriteLine("Hello  " + name + " " + "Go Bus!");
 
+                        Console.WriteLine("Press any key to exit the program...");
 
+                        Console.ReadKey(true);
 
+                        
+
+                }// end of try
+
+                catch
+
+                {
+
+                    Console.WriteLine("Please enter your name and try running the program again...");
+
+                    Console.WriteLine("Press any key to exit the program...");
+
+                    Console.ReadKey(true);
+
+                }// End of catch
+
+            }// End of Main
+
+        }// End of Class
+
+    }// End of namesplace
