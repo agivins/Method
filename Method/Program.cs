92843fc0f0c9c1c62796a2ac74b06ad92e062a55
Alexandra Givins <agivins@usf.edu>
2020-09-24 18:45:54 -0700

    Name Method

--- Method/Program.cs
+++ Method/Program.cs
@@ -1,26 +1,33 @@
-﻿using System;
+﻿/*ISM 4300 Console Program- Submitted by Alexandra Givins
+ Comments: This code demonstrates the use of Method statements after getting input from users. 
+*/
+using System;
 
 namespace PC
 {
     class Program
     {
-        static void UserName()
+        static void MyName()
         {
             Console.WriteLine("Please enter your name..");
         }
         static void Main(string[] args)
         {
+            //the value of user input 
             Console.WriteLine("Please enter your name..");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name + "  " + " Go Bus!");
+            //Here is the for the use input 
+            Console.WriteLine("Hello" + name + "-" + "Go Bus!");
             {
                 Console.WriteLine(" The Program enter your name.");
+                // Pause the program and wait for the user to press a key to end the program
                 Console.WriteLine("Press any key to exit the program...");
                 Console.ReadKey(true);
             }
         }
     }
 }
+
 
 
 
